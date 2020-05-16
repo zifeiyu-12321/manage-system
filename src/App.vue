fafb9d757dce8b1eeb6f05abd9677381ba1f3326
feiyu <2462858146@qq.com>
2020-05-16 17:42:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    666666666666666666666666666
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
